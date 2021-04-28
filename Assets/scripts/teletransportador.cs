c5ff99b48283b14c1cce3774be35dead9c64631e
oscar <82243536+OscarDavidGonzalezAngarita@users.noreply.github.com>
2021-04-28 08:45:24 -0500

    a
    a

---
+++ Assets/scripts/teletransportador.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teletransportador : MonoBehaviour
+{
+    public GameManager gm;
+    void Start()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gm.activar = true;
+        }
+    }
+}
